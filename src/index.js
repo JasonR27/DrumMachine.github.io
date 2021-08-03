5e1635941638d0cba9d1d636b4e995512a9cf0d6
JasonR27 <jeisonroblero980@gmail.com>
2021-08-02 20:25:40 -0600

    RandomQuoteMachine

--- src/index.js
+++ src/index.js
@@ -1,11 +1,6 @@
+import React from 'react'
 import ReactDOM from 'react-dom'
 import AppWrapper from "./App"
-import { App } from "./App"
 
-//ReactDOM.render(<AppWrapper />, document.getElementById("root"));
-
-ReactDOM.render(
-    <h1>Hello, world!</h1>,
-    document.getElementById('root')
-);
+ReactDOM.render(<AppWrapper />, document.getElementById("App"));
 
